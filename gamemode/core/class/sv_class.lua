bd15a23e37c5969b2a2cc01d57f6a8ff7f5d1a80
LZ1WS <The-Serpent-Hand@yandex.ru>
2022-07-23 22:14:04 +0500

    1.1.2-beta
    | Added |
    - Ability sounds to Priest
    - New Survivors: Priest, Journalist, Addicted
    #a priest who has a holy book. they can stay still and charge a prayer that will heavily debuff a killer for 10 seconds, with stuff like blurred/double vision, lower movement speed, and disabling their abilities.#
    #a photographer with a flash camera on cool down that uses the light of the camera flash to blind the killer for a few seconds#
    #A stimulant addicted survivor who has a lot less health due to their frail body, but can use an active ability to speed them up a limited amount of times by consuming the drug#
    - Animations to the Hub F4 Menu
    - Sounds for the buttons in Hub F4 Menu
    - Slightly changed Hub F4 Menu
    - An access type in ulx which grants the ability for the usergroup to set a survivor from a F4 menu
    - Holy Weaken Effect
    
    | Changed |
    - Moved icons to the main gamemode content folder
    - Reworked survivors abilities in code to make them easier to implement
    - Now Hub F4 Menu uses an core gamemode table of Survivors, instead of the custom manual one
    
    | Fixed |
    - Steve's Ability (minor bug)
    
    Maybe something else I forgot.

--- gamemode/core/class/sv_class.lua
+++ gamemode/core/class/sv_class.lua
@@ -34,6 +34,7 @@ function playermeta:SetSurvClass(class)
 	self:SetRunSpeed(GM.CLASS.Survivors[class].runspeed)
 	self:SetMaxHealth(GM.CLASS.Survivors[class].life)
 	self:SetHealth(GM.CLASS.Survivors[class].life)
+	self:SetNWInt("sls_addicted_shots", 3)
 	self:GodDisable()
 	--self:SetNWInt("ClassID", class)
 	self.ClassID = class
@@ -185,58 +186,10 @@ end
 end
 end)
 
-util.AddNetworkString("SteveAbil_WH")
-	hook.Add("PlayerButtonDown", "SteveAbil_Bind", function(ply, button)
-		if !IsFirstTimePredicted() then return end
-if ply:GetNWBool("steveabilused", false) == false then
-	if ply.ClassID == 10 and ply:Alive() and button == KEY_Q then
-ply:SetNWBool("steveabilused", true)
-ply.SteveResist = true
-net.Start("SteveAbil_WH")
-net.WriteTable({ply})
-net.Send(GM.ROUND.Killer)
-	net.Start( "notificationSlasher" )
-	net.WriteTable({"class_ability_used"})
-	net.WriteString("safe")
-	net.Send(ply)
-for _,v in pairs(ents.FindInSphere(ply:GetPos(), 300)) do
-if v:IsPlayer() and v:Alive() and v:Team() == TEAM_SURVIVORS then
-v.SteveResist = true
-	net.Start( "notificationSlasher" )
-	net.WriteTable({"steve_resist_used"})
-	net.WriteString("safe")
-	net.Send(v)
-end
-end
-timer.Simple(15, function()
-for _,players in pairs(player.GetAll()) do
-if players.SteveResist then
-players.SteveResist = false
-	net.Start( "notificationSlasher" )
-	net.WriteTable({"steve_resist_off"})
-	net.WriteString("safe")
-	net.Send(player)
-end
-end
-end)
-	timer.Simple(30, function()
-ply:SetNWBool("steveabilused", false)
-	net.Start( "notificationSlasher" )
-	net.WriteTable({"class_ability_time"})
-	net.WriteString("safe")
-	net.Send(player)
-end)
-end
-end		
-	end)
-
 hook.Add("sls_round_PreStart", "sls_sability_PreStart", function()
 for _, ply in pairs(player.GetAll()) do
 if ply.ClassID == 10 and ply:Alive() and IsValid(ply) then
 ply:SetNWBool("steveabilused", false)
 end
 end
-end)
-	hook.Add("sls_round_End", "sls_sability_End", function()
-hook.Remove("PlayerButtonDown", "SteveAbil_Bind")
 end)
