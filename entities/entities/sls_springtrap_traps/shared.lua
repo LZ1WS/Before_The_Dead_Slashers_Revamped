a1bb0fe29103f09215b367ca4eafb105763d90af
LZ1WS <94416111+LZ1WS@users.noreply.github.com>
2023-02-06 19:25:16 +0500

    Delete entities directory

--- entities/entities/sls_springtrap_traps/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type			= "anim"
-ENT.Base			= "prop_effect"
-ENT.PrintName		= "Phantom Trap"
-ENT.Author			= "L.Z|W.S"
-ENT.Information		=  ""
-ENT.Category		= "Slashers"
-ENT.Spawnable		= true
-ENT.AdminSpawnable	= false
--- entities/entities/tfbow_arrow_stuck/init.lua
+++
@@ -1,5 +0,0 @@
-AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
-AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
- 
-include('shared.lua')
-
