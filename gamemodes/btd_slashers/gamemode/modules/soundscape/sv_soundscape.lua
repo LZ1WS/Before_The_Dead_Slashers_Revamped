b452d66001b6c453935570852b2a5290951a5e79
Serpent <94416111+LZ1WS@users.noreply.github.com>
2026-08-27 13:35:16 +0500

    fix(soundscape): Guard

--- gamemodes/btd_slashers/gamemode/modules/soundscape/sv_soundscape.lua
+++ gamemodes/btd_slashers/gamemode/modules/soundscape/sv_soundscape.lua
@@ -12,9 +12,11 @@ local GM = GM or GAMEMODE
 -- Son à la mort d'un joueur
 local function PlayerDeath(victim, inflictor, attacker)
 	if victim:Team() == TEAM_SURVIVORS then
-		for _, v in ipairs(player.GetAll()) do
-			if victim.ClassID != 0 then
-				v:PlaySound(GM.CLASS.Survivors[victim.ClassID].die_sound)
+		local class = victim.ClassID and GM.CLASS.Survivors[victim.ClassID]
+
+		if class and class.die_sound then
+			for _, v in ipairs(player.GetAll()) do
+				v:PlaySound(class.die_sound)
 			end
 		end
 	end
